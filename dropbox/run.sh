0ba144f8989853f6ad4429618294a8d9e914967f
Kyle A Anderson <kyleaa@gmail.com>
2015-02-02 19:55:41 -0500

    Switched from useradd to adduser (docker issue #6345)

--- dropbox/run.sh
+++ dropbox/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-useradd --gid=0 --uid=$DROPBOX_UID --home=/home/dropbox --shell=/bin/bash dropbox
+adduser --gecos "Dropbox" --disabled-password --gid=0 --uid=$DROPBOX_UID --home=/home/dropbox --shell=/bin/bash dropbox
 chown -R dropbox:root /home/dropbox
 su -l dropbox /home/dropbox/.dropbox-dist/dropboxd
