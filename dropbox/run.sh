b904433030123499a4f9811bb1fb87d220cf0b4f
Kyle A Anderson <kyleaa@gmail.com>
2015-03-26 12:21:46 -0400

    fix permissions on dropbox folders only create dropbox user if it doesn't exist

--- dropbox/run.sh
+++ dropbox/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-adduser --gecos "Dropbox" --disabled-password --gid=0 --uid=$DROPBOX_UID --home=/home/dropbox --shell=/bin/bash dropbox
+id -u dropbox &>/dev/null || adduser --gecos "Dropbox" --disabled-password --gid=0 --uid=$DROPBOX_UID --home=/home/dropbox --shell=/bin/bash dropbox
 chown -R dropbox:root /home/dropbox
+chmod -R u+rw /home/dropbox/Dropbox /home/dropbox/.dropbox /home/dropbox/.dropbox-master
 su -l dropbox /home/dropbox/.dropbox-dist/dropboxd
